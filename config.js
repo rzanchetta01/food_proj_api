e9e752f8f932f1c1ab4d570ab68e08bce2bd8d5f
Rodrigo Zanchetta <rzanchetta01@gmail.com>
2022-05-31 16:42:23 -0300

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ app.use(
 )
 
 db
-.connect('mongodb+srv://admin:aiJ9uIFNBJeOfL0O@apirpg.zmnvg.mongodb.net/Foods_Proj?retryWrites=true&w=majority')
+.connect('mongodb://rzanchetta02:RqYdvnAtHiYUbXJkgexAR7bzqA2qE6BFy5N0lj9pZX5s8gcgNJE20EDDcUuUoftDTpI3C72tQPhJRHrytP43iQ==@rzanchetta02.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@rzanchetta02@')
 .then(
     console.log('API -- DB CONNECTED -- API ONLINE'),
     app.listen(3001)
